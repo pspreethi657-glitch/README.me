e70f0fd8f0dd85c345e2b8570fdf45751afff2e9
preethi <pspreethi657@gmail.com>
2026-09-06 15:42:57 +0530

    Add files via upload

---
+++ image_generator.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from diffusers import DiffusionPipeline
+st.title("🎨 AI Image Generator")
+prompt = st.text_input(
+    "Enter your prompt:",
+    "A cute cat sitting in a garden"
+)
+if st.button("Generate Image"):
+    with st.spinner("Generating image..."):
+        pipe = DiffusionPipeline.from_pretrained( "sd-research/stable-diffusion-2-1-base")
+        image = pipe(prompt).images[0]
+    st.image(image, caption="Generated Image")
